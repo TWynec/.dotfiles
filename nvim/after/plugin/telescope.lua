22744ac96914a0d35bff4db8970780f5d428f512
Trace Wynecoop <trwyne03@gmail.com>
2023-06-04 22:56:12 -0700

    Fix remap

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -16,3 +16,10 @@ require("telescope").setup({
 })
 
 require("telescope").load_extension "file_browser"
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<space>pv",
+  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { noremap = true }
+)
