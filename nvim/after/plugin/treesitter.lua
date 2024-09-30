a6170bc1f5ba633325f142786a00f4ab67696a87
Trace <59637987+TWynec@users.noreply.github.com>
2024-09-30 13:59:00 -0700

    Update treesitter.lua

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "python", "lua", "html", "css" },
+  ensure_installed = { "javascript", "python", "lua", "html", "css" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
